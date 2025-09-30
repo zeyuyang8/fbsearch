ee3cc15ac39491307bb30a1129849289d23ec676
Zeyu Yang <zy45@meta.com>
2025-09-30 13:10:31 -0700

    update

--- fbsearch/__init__.py
+++ fbsearch/__init__.py
@@ -5,3 +5,8 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 import warnings
 
 warnings.filterwarnings("ignore", module="torch.distributed.distributed_c10d")
+
+TRANSFORMERS_PATH_MAP = {
+    "llama-3.2-1b-instruct": "meta-llama/Llama-3.2-1B-Instruct",
+    "llama-3.1-8b-instruct": "meta-llama/Llama-3.1-8B-Instruct",
+}
